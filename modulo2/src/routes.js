ac474e0b9f01fb3ad2d3b04fe416e889db210c8c
Cristiane Faria <cristiane.trc@gmail.com>
2019-09-11 23:00:48 -0300

    loader de models com teste de insercao

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello rocket' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Cristiane Faria',
+    email: 'cristiane@bitup.com.br',
+    password_hash: '20934839048',
+  });
+  return res.json(user);
+});
 
 export default routes;
