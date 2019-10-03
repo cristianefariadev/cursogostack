a6a22682c66a6b81dcd6d5c68c168a33a4b78e9c
cristiane.trc@gmail.com <cristiane.trc@gmail.com>
2019-10-03 20:30:26 -0300

    Listagem de prestadores de serviço

--- modulo3/src/app/models/User.js
+++ modulo3/src/app/models/User.js
@@ -27,7 +27,7 @@ class User extends Model {
 
   // belognsto = pertence a model de usuario pertence ao model de file
   static associate(models) {
-    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
   }
 
   checkPassword(password) {
