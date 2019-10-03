141d3a2e175bb56b52f4212b09719c5f46bc6c96
cristiane.trc@gmail.com <cristiane.trc@gmail.com>
2019-10-03 19:57:42 -0300

    Avatar do usuário

--- modulo3/src/routes.js
+++ modulo3/src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,18 +18,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.posts('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
-
-// rota teste
-// routes.get('/', async (req, res) => {
-//   const user = await User.create({
-//     name: 'Cristiane Faria',
-//     email: 'cristiane@bitup.com.br',
-//     password_hash: '20934839048',
-//   });
-//   return res.json(user);
-// });
