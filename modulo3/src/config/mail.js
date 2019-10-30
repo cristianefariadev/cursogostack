2d218eee5813ebe60911c534ed9dc846c86ee71b
cristiane.trc@gmail.com <cristiane.trc@gmail.com>
2019-10-30 17:35:32 -0300

    Configurando Nodemailer

---
+++ modulo3/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'a3e2b6c9f1af22',
+    pass: 'bdb7e44e310086',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
